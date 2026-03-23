6976dcc42d72dec939ea49023019c014aa4f008c
Saugat <sau.gat8602@gmail.com>
2026-03-23 15:57:55 +0545

    Refactored the input take.

--- number-guesser/src/numberGuesser/UserInterface.java
+++ number-guesser/src/numberGuesser/UserInterface.java
@@ -9,97 +9,96 @@ public class UserInterface {
     int difficulty = 0;
 
 
-
-    public UserInterface(Scanner scan){
+    public UserInterface(Scanner scan) {
         this.scan = scan;
         this.logic = new Logic();
     }
 
-    public void startGame(){
+    public void startGame() {
         String newGame = "";
-        do{
+        do {
             printDifficulty();
-            difficulty = getDifficulty();
+            difficulty = getInput();
             logic.difficulty(difficulty);
-            System.out.println("You've chosen Difficulty: "+ difficulty);
+            System.out.println("You've chosen Difficulty: " + difficulty);
             String playon = "";
-            do{
+            do {
                 guesses();
                 System.out.println("Want to play on, new Game or quit?, Y/G/N?");
                 playon = scan.nextLine();
-            }while(playon.equals("Y"));
+            } while (playon.equals("Y"));
             newGame = playon;
-        }while(newGame.equals("G"));
+        } while (newGame.equals("G"));
         System.out.println("Game Over!!");
 
     }
 
-    public void printDifficulty(){
+    public void printDifficulty() {
         System.out.println("Enter Difficulty:");
         System.out.println("1: Easy (1-50)");
         System.out.println("2: Moderate (1 -100)");
         System.out.println("3: Hard (1 -200");
     }
 
-    public int getDifficulty(){
+    public int getInput() {
+        int input = 0;
+        int bound = 0;
+
+        switch (difficulty) {
+            case 0:
+                bound = 4;
+                break;
+            case 1:
+                bound = 50;
+                break;
+            case 2:
+                bound = 100;
+                break;
+            case 3:
+                bound = 200;
+                break;
+        }
         boolean run = true;
-        while(run){
-            try{
-                while(!(difficulty>0 && difficulty<4)){
-                    difficulty = Integer.parseInt(scan.nextLine());
-                    if(!(difficulty>0 && difficulty<4)){
+        while (run) {
+            try {
+                while (!(input > 0 && input < bound)) {
+                    input = Integer.parseInt(scan.nextLine());
+                    if (!(input > 0 && input < bound)) {
                         System.out.println("Please enter a valid selection!");
                         continue;
                     }
                     run = false;
                 }
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Enter a valid number.");
-            }
-        }
-
-        return difficulty;
-    }
-
-
-
-    public int takeInput(){
-        int input = 0;
-        boolean run = true;
-        while(run){
-            try{
-                input = Integer.parseInt(scan.nextLine());
-                run = false;
-            }catch (Exception e){
-                System.out.println("Enter a valid number!");
             }
         }
         return input;
     }
 
-    public void guesses(){
+    public void guesses() {
         int chances = 8;
-        do{
-            System.out.println("Take a guess. You have " + chances +" tries.");
-            int input = takeInput();
-            if(logic.takeGuess(input)){
+        do {
+            System.out.println("Take a guess. You have " + chances + " tries.");
+            int input = getInput();
+            if (logic.takeGuess(input)) {
                 System.out.println("Right Guess.");
                 break;
             }
             closeness(input);
             chances--;
-        }while(chances != 0);
+        } while (chances != 0);
 
     }
 
-    public void closeness(int guess){
-         int toGuess = logic.getToGuess();
-         int closeness = guess - toGuess;
-         if(Math.abs(closeness) <= 5){
-             System.out.println("You are less than or about 5 steps away.");
-         }else{
-             System.out.println((closeness < 0) ? "Try a bit higher" : "Try a bit lower.");
+    public void closeness(int guess) {
+        int toGuess = logic.getToGuess();
+        int closeness = guess - toGuess;
+        if (Math.abs(closeness) <= 5) {
+            System.out.println("You are less than or about 5 steps away.");
+        } else {
+            System.out.println((closeness < 0) ? "Try a bit higher" : "Try a bit lower.");
         }
     }
 
